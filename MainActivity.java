831915879a0fafd1aff09f3a2a60a6f6fae432f2
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-05-10 20:41:03 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.customerregistration;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
+    private EditText dateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initDatePicker();
+        dateButton = findViewById(R.id.datePicker);
+        // Set the default text to "Select date"
+        dateButton.setText("Select date");
+        // Set onClickListener to open DatePicker dialog
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDatePicker(view);
+            }
+        });
     }
+
 }
