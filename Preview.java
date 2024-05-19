c6c62d608a77f16c7775aac6c56ce79e4593e219
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-05-20 01:48:20 +0300

    Update Preview.java

--- Preview.java
+++ Preview.java
@@ -1,12 +1,22 @@
 package com.example.customerregistration;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 public class Preview extends AppCompatActivity {
 
@@ -67,6 +77,82 @@ public class Preview extends AppCompatActivity {
             // Set the image URI to the ImageView
             Uri imageUri = Uri.parse(intent.getStringExtra("imageUri"));
             imageView.setImageURI(imageUri);
+
+            Button saveButton = findViewById(R.id.saveBtn);
+            saveButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(Preview.this);
+                    builder.setMessage("Registered successfully");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Start the Display activity
+                            Intent intent = new Intent(Preview.this, Display.class);
+                            // Pass any data you need to the Display activity here
+                            startActivity(intent);
+                            // Finish the Preview activity
+                            finish();
+                        }
+                    });
+                    builder.show();
+                }
+            });
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.exit) {
+            showExitDialog();
+            return true;
+        }
+        if (id == R.id.amh) {
+            setLocale("am");
+            return true;
+        }
+        if (id == R.id.eng) {
+            setLocale("en");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showExitDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to exit?");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Exit the app
+                finishAffinity();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Dismiss the dialog
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Resources resources = getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+        recreate();
     }
 }
